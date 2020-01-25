f079d3ecdc407ddf2c89b33f77060ec99d571bee
ehernandez <edgardjaime@gmail.com>
2020-01-25 18:25:31 -0500

    EJHM Linea Base

---
+++ src/main/java/com/pe/serfor/curso_selenium_maven/App.java
@@ -0,0 +1,13 @@
+package com.pe.serfor.curso_selenium_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
